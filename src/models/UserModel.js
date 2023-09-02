6b808015d0c87507a68e45c9b1b36547c7cf68ac
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2023-09-02 22:30:29 +0600

    Models Created

---
+++ src/models/UserModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    address: String,
+    phoneNumber: String,
+},
+    { timestamps: true , versionKey: false});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
